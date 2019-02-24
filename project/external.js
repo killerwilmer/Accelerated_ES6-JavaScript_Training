fcff70fe4acd5f87b305bca9064789bf48f88b52
Wilmer Arteaga <killerwilmer@gmail.com>
2019-02-24 16:31:47 -0500

    Modules basics

---
+++ project/external.js
@@ -0,0 +1,7 @@
+let keyValue = 1000;
+function test() {
+  keyValue = 20000;
+  console.log('tested !');
+}
+
+export {keyValue, test};
---
+++ project/script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// Write ES6 code here...
+import {keyValue, test} from './external.js';
+
+console.log(keyValue);
+test();
+console.log(keyValue);
